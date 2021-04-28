e4075126696364e08377451d5f21cb0d11ab7720
王圣杰 <wangsj@youchebao.cn>
2021-04-28 14:25:58 +0800

    java8

---
+++ matrix-daily/src/main/java/com/sj/daily/java8/NewJava.java
@@ -0,0 +1,32 @@
+package com.sj.daily.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ShengJie.Wang
+ * @date 2021-04-23 14:41
+ */
+public class NewJava {
+
+  // 使用 java 8 排序
+  private void sortUsingJava8(List<String> names) {
+    Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+  }
+
+  public static void main(String args[]) {
+    NewJava  test = new NewJava() ;
+    List<String> names1 = new ArrayList<String>();
+    names1.add("Google ");
+    names1.add("Runoob ");
+    names1.add("Taobao ");
+    names1.add("Baidu ");
+    names1.add("Sina ");
+    test.sortUsingJava8(names1) ;
+    Arrays.asList( "a", "c", "b" ).forEach( ( String e ) -> System.out.println( e ) );
+  }
+
+
+}
